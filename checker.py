a14dd3fcde3c2330ee8e89b88b086dd16c7eca53
Tyrao <66280234+Tyrao@users.noreply.github.com>
2020-11-18 01:49:58 +0000

    Checker Is Finished

--- checker.py
+++ checker.py
@@ -1,4 +1,4 @@
-import random, string, aiohttp, asyncio
+import random, string, aiohttp, asyncio, logging, aiofiles, time
 from colorama import *
 init()
 
@@ -13,7 +13,8 @@ asyncio.set_event_loop(loop)
 class Github:
     def __init__(self):
         self.length = 3 # This is the length of the username to generate, change this to what ever you would like.
-        self.api_github = aiohttp.ClientSession()
+        logging.getLogger('asyncio').setLevel(logging.CRITICAL)
+        open("./available.txt", "w").close()
 
     def get_unique_username(self, amount_to_gen):
         usernames = []
@@ -25,15 +26,20 @@ class Github:
         return usernames
 
     async def check_username(self, username):
+        self.api_github = aiohttp.ClientSession()
         try:
-            send_data = await self.api_github.get(f"https://github.com/users/{username}")
+            send_data = await self.api_github.get(f"https://github.com/{username}", timeout=5)
+            await asyncio.sleep(0.25)
             if send_data.status == 200:
                 print(Fore.RED+f"[{username}] is taken")
             elif send_data.status == 404:
                 print(Fore.GREEN+f"[{username}] is available")
+                async with aiofiles.open("./available.txt", mode="a") as workingAccount:
+                    await workingAccount.write(f"{username}\n")
+            else:
+                pass
         except:
             pass
-
 
 
 def chunks(lst, n):
@@ -42,9 +48,9 @@ def chunks(lst, n):
 
 if __name__ == "__main__":
     _instance = Github()
-    amount_to_gen = int(input(Fore.WHITE+"Enter amount to of usernames to generate and check: "))
+    amount_to_gen = int(input("Enter amount to of usernames to generate and check: "))
     account = _instance.get_unique_username(amount_to_gen)
-    for chunk in chunks(account, amount_to_gen):
+    for chunk in chunks(account, 350):
         tries = asyncio.gather(*[_instance.check_username(username) for username in chunk])
         loop.run_until_complete(tries)
-        asyncio.sleep(0.25)
+        time.sleep(0.25)
